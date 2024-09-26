08a5f9389ce42433b3610128c6ff79fedd94e5ce
Paulo <git@pauloo27.dev>
2024-09-25 21:05:28 -0300

    ref: reformat

--- index.ts
+++ index.ts
@@ -3,24 +3,24 @@ class RawValue {
 }
 
 export function raw(value: unknown): RawValue {
-  return new RawValue(value)
+  return new RawValue(value);
 }
 
 export function uri(strings: TemplateStringsArray, ...values: any[]): string {
   // use a array as "string builder" because, guess what, js std::lib sucks!
-  const builder = new Array(strings.length + values.length)
+  const builder = new Array(strings.length + values.length);
 
   let valueIdx = 0;
 
   for (const str of strings) {
-    builder.push(str)
+    builder.push(str);
     if (valueIdx < values.length) {
       const value = values[valueIdx++];
       builder.push(
-        value instanceof RawValue ? value.raw :encodeURIComponent(value),
-      )
+        value instanceof RawValue ? value.raw : encodeURIComponent(value),
+      );
     }
   }
 
-  return builder.join('')
+  return builder.join("");
 }
